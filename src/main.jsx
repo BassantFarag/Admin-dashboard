4165e9597eee608d5eb29e2a83fa03b50dedde52
m-ibrahim-x <muhammedibrahim2268@gmail.com>
2026-09-04 16:33:55 +0300

    feat: complete users page logic and loading UI

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import React from 'react';
 import './index.css'
 import App from './App.jsx'
 import AuthProvider from './contexts/AuthProvider.jsx'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
+      <ToastContainer />
     </AuthProvider>
   </React.StrictMode>
 )
